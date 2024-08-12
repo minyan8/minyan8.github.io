f115926aa245267d5beaf48be606ac195942e989
Min Yan <miny13@mcmaster.ca>
2024-08-12 14:09:04 -0400

    update the email

--- src/components/Contact/EmailLink.js
+++ src/components/Contact/EmailLink.js
@@ -82,7 +82,7 @@ const EmailLink = ({ loopMessage }) => {
       onMouseEnter={() => setIsActive(false)}
       onMouseLeave={() => idx < messages.length && setIsActive(true)}
     >
-      <a href={validateText(message) ? `mailto:${message}@gmail.com` : ''}>
+      <a href={validateText(message) ? `mailto:${ryanm.sde}@gmail.com` : ''}>
         <span>{message}</span>
         <span>@gmail.com</span>
       </a>
