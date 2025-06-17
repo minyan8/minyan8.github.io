e5caddb9bc56b21b8aee9012c29fb1b8eceb4284
Min Yan <miny13@mcmaster.ca>
2025-06-17 17:13:05 +0800

    modify pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/minyan8/minyan8.github.io.git";
 
 ghpages.publish(
   pathname,
